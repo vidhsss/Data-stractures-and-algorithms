f625f67ef7cf0ec38dbe040d23b43753ff847891
vidhsss <60235738+vidhsss@users.noreply.github.com>
2021-07-11 00:00:21 +0530

    Create camel.cpp

---
+++ camel.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	int m;
+
+ char str[1000];
+cin>>str;
+int i=0;
+  for (  i=0;str[i+1]!='\0';i++)
+  {
+      m=str[i+1]-str[i];
+     cout<<str[i]<<m;
+  }
+
+cout<<str[i];
+
+
+ return 0;
+}

dcb5f339ce176773110c065565243ff8b9e69602
vidhsss <60235738+vidhsss@users.noreply.github.com>
2021-07-10 23:52:20 +0530

    Create string_sort.cpp

---
+++ string_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+bool compare(string a, string b)
+{
+    if(a.length()==b.length())
+        return a<b;
+    return a.length()>b.length();
+}
+int main()
+{
+    string s[100];
+    int n;
+    cin>>n;
+    cin.get();
+    for(int i=0;i<n;i++)
+    {
+        getline(cin,s[i]);
+    }
+    sort(s,s+n,compare);
+    for(int i=0;i<n;i++)
+    {
+        cout<<s[i]<<endl;
+    }
+}

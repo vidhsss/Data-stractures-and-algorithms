54bbcfb80504a8341cc7bfd51b0c2ce4f938ebf5
vidhsss <60235738+vidhsss@users.noreply.github.com>
2021-07-10 23:53:18 +0530

    Create fundamental.cpp

---
+++ fundamental.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<limits>
+using namespace std ;
+int main()
+{
+    signed CodingBlocks = 9;
+ signed Nagarro = A;
+ signed char HackerBlocks = 'A';
+cout<<CodingBlocks<<endl;
+cout<<HackerBlocks<<endl;
+
+    
+
+    return 0;
+
+}

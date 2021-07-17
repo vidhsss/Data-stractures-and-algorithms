6761f8506eea7289d289d09ea00b9a2588ae0280
vidhsss <60235738+vidhsss@users.noreply.github.com>
2021-07-17 23:46:28 +0530

    Create bubble.cpp

---
+++ sorting/bubble.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void bubblesort(int a[],int n)
+{
+	
+	for(int i=0;i<n-1;i++)
+		{
+			for(int j=0;j<n-i-1;j++)
+				{
+					if(a[j]>a[j+1])
+						{
+							swap(a[j],a[j+1]);
+						}					
+				}
+		}
+	
+	
+}
+
+int main()
+	{
+		
+	int n;
+	cin>>n;
+	
+	int a[n];
+	for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+
+	bubblesort(a,n);
+	for(int i=0;i<n;i++)
+		{	
+			cout<<a[i]<<endl;
+			
+		
+		}
+	
+	return 0;	
+	}
